1754d310fec9c3b96d38f2fcceb4c504887c29c3
Jojih <joannadesouzanartins@gmail.com>
2026-03-11 17:15:42 -0300

    Frutinhas.11.03.26
    Desgraça de teste

---
+++ 11-03-2026/src/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// //const alunos = [
+//   {id: 1, nome: "Joh", idade: 18},
+//   {id: 2, nome: "Saara", idade: 17},
+//   {id: 3, nome: "Nicolly", idade: 15},
+// ]*//
+
+let frutas = [
+  {id: 1, nome: "Cereja", cor: "Vermelha"},
+  {id: 2, nome: "Uva", cor: "Roxa"},
+  {id: 3, nome: "Laranja", cor: "Laranja"},
+  {id: 4, nome: "Limão", cor: "Verde"},
+]
+
+//app.get('/', (req, res) => {
+ // res.send('Hello World!')
+//})
+
+app.get('/alunos/getAll', (req, res) => {
+  res.json({
+       sucess: true,
+       data: alunos
+  })
+})
+
+app.get('/frutas/getAll', (req, res) => {
+  res.json({
+       sucess: true,
+       data: frutas
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
